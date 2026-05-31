3ea600980429ac6a65c5fcdb2bff199bc54f1ed7
Wassim Amrani <wassimamrani@live.fr>
2026-05-30 23:44:30 -0400

    created the response model clas and inti

--- main.py
+++ main.py
@@ -2,11 +2,16 @@ from dotenv import load_dotenv
 from pydantic import BaseModel
 from langchain_openai import ChatOpenAI
 from langchain_anthropic import ChatAnthropic
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
 
 load_dotenv()
-
+class ResearchResponse(BaseModel):
+    topic: str
+    summary : str
+    sources: list[str]
+    tools_used: list[str]
 # setting up the LLM
 
 llm = ChatOpenAI(model = "gpt-4.1-mini")
-response = llm.invoke("What is the mening of life?")
-print(response.content)
+paser = PydanticOutputParser(pydantic_object = ResearchResponse)
