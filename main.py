c20b050e2a0a21da2ea1d10fc8d6a8ae7ba5b98e
Wassim Amrani <wassimamrani@live.fr>
2026-07-07 18:25:13 -0400

    finished implementing the tools section, wikipedia did not work, created a personal tool to save the output as a .txt file

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from langchain_anthropic import ChatAnthropic
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import PydanticOutputParser
 from langchain_classic.agents import create_tool_calling_agent, AgentExecutor
-from tools import search_tool
+from tools import search_tool, wiki_tool, save_tool
 
 load_dotenv()
 class ResearchResponse(BaseModel):
@@ -31,11 +31,11 @@ prompt = ChatPromptTemplate.from_messages(
         ),
         ("placeholder", "{chat_history}"),
         ("human", "{query}"),
-        ("placeholder," "{agent_scratchpad}"),
+        ("placeholder", "{agent_scratchpad}"),
     ]
 ).partial(format_instructions = parser.get_format_instructions())
 
-tools = [search_tool]
+tools = [search_tool, save_tool]
 agent = create_tool_calling_agent(
     llm = llm,
     prompt = prompt,
@@ -43,10 +43,12 @@ agent = create_tool_calling_agent(
 )
 
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose = True)
-raw_response = agent_executor.invoke({"query": "How many rings does Michael Jordan have?"})
+query = input("What can I help you research? ")
+raw_response = agent_executor.invoke({"query": query})
 print(raw_response)
 
 try:
     structured_response = parser.parse(raw_response.get("output"))
+    print(structured_response)
 except Exception as e:
     print("Error parsing response", e, "Raw Response - ", raw_response)
